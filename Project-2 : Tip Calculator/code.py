bb4e4c21dc4c617bbb768edd6b88f9a9f5da929f
Yasar Ali Shaik <yasarshaik0806@gmail.com>
2025-08-21 02:50:42 +0530

    Update code.py

--- Project-2 : Tip Calculator/code.py
+++ Project-2 : Tip Calculator/code.py
@@ -1,1 +1,16 @@
+print("Welcome to the tip calculator!\n\n")
 
+# Input and type declerations
+bill = float(input("How much is the total bill?\n$"))
+tip = int(input("How much tip would you like to give?(10, 12 or 15)\n"))
+frnds = int(input("How many people will split the bill?\n"))
+
+# Calculation Processes
+tip_percent = tip / 100
+tip_amount = bill * tip_percent
+total_bill = bill + tip_amount
+bill_for_each = total_bill / frnds
+final_amount_pp = round(bill_for_each, 2)
+
+# Output.
+print(f"\nEach person should pay - ${final_amount_pp}")
